86006583580980377c4bb68d6d6408897034529a
ChanTaeMin <98499774+ChanTaeMin@users.noreply.github.com>
2023-05-03 20:43:23 -0400

    Add files via upload

---
+++ rename.py
@@ -0,0 +1,9 @@
+import os
+ 
+src_path = 'C:/Users/Robert Chiegil/Downloads'
+
+dest_path = 'C:/Users/Robert Chiegil/OneDrive/Desktop'
+
+os.rename('main.txt', "NiceName.txt")
+
+print("Renamed Successfully")
